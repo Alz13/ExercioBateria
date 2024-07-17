2aa7b67ca06fd12c1060fddc1c398a3dae30fee9
Alexandre <127051673+Alz13@users.noreply.github.com>
2024-07-17 16:42:21 -0300

    Add files via upload

---
+++ Bateria/Celula.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bateria
+{
+    internal class Celula
+    {
+        private int carga { get;  set; }
+        private static int cargaBateria {get; set;}
+
+        public Celula()
+        {
+            this.carga = 25;
+            cargaBateria += 25;
+        }
+
+
+
+        public void setCargabateria(int cargabateria)
+        {
+           cargaBateria = cargabateria;
+        }
+
+        public void consumir()
+        {
+            if(cargaBateria > 0 && this.carga > 0) { this.carga -= 5; }
+        }
+
+
+        public void carregar()
+        {
+            if(cargaBateria < 100 && this.carga < 100)
+            this.carga += 5;
+        }
+
+    }
+}
---
+++ Bateria/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Bateria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ale = new Random();
+            ConsoleKeyInfo a;
+            Celula[] cel = new Celula[4];
+            cel[0] = new Celula();
+            cel[1] = new Celula();
+            cel[2] = new Celula();
+            cel[3] = new Celula();
+
+            do
+            {
+ 
+                int bateria = ale.Next(4);
+                int qual = ale.Next(2);
+
+                if (qual == 0 && cel[bateria].getCargaBateira() > 0){cel[bateria].consumir(); }
+                else if(cel[bateria].getCargaBateira() <= 100){ cel[(bateria)].carregar();}
+
+                Console.WriteLine("Carga da bateria 1:" + cel[0].getCarga());
+                Console.WriteLine("Carga da bateria 2:" + cel[1].getCarga());
+                Console.WriteLine("Carga da bateria 3:" + cel[2].getCarga());
+                Console.WriteLine("Carga da bateria 4:" + cel[3].getCarga());
+                int total = cel[0].getCarga() + cel[1].getCarga() + cel[2].getCarga() + cel[3].getCarga();
+                cel[1].setCargabateria(total);
+
+                Console.WriteLine("Carga total:" + cel[0].getCargaBateira());
+
+                Console.WriteLine("Pressione 'F' para sair ou qualquer outra tecla para continuar:");
+                a = Console.ReadKey();
+                Console.Clear();
+
+            } while (a.Key != ConsoleKey.F);
+            
+
+        }
+    }
+}
